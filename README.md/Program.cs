c36722415d0bf40b3a4ed39aafb68788f738fec5
hobao-133 <kevinbh133@gmail.com>
2024-11-19 21:34:51 +0700

    first commit

---
+++ README.md/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class Program{
+    static void Main (string[]args){
+        Console.WriteLine("Enter your name: ");
+        string yourName = Console.ReadLine();
+        Console.WriteLine("Hello: "+ yourName);
+    }
+}
